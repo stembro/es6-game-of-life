40d430e1447268aee248498e293907f2119ec413
Stephen Brown <stembro@Stephens-MacBook-Air.local>
2017-01-14 22:41:05 -0700

    added style.css back, need to figure out how i want to handle css files with webpack. rebuilt app for deployment

--- tools/distServer.js
+++ tools/distServer.js
@@ -4,7 +4,7 @@ import open from 'open';
 import chalk from 'chalk';
 
 const port = process.env.PORT || 3000;
-const host = process.env.IP || 'http://localhost';
+const host = process.env.IP || 'localhost';
 
 const app = express();
 
